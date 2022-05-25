d45bc35f8a7cdea64e5dc6baac4b90aaa413586c
Brian Marth <marthbrian@gmail.com>
2022-05-25 15:33:03 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FirewallForm from './components/firewall';
+
+function App() {
+  return (
+    <div class="main">
+      <FirewallForm></FirewallForm>
+    </div>
+  );
+}
+
+export default App;
